a9f330a58374add0b62a51d55e2d94b4efdac3c3
Miroslav <miroslav.pivovarsky@iklub.sk>
2014-05-14 18:52:00 +0200

    ulozenie stranky na SD kartu

--- zasuvka.ino
+++ zasuvka.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
+#include <SD.h>
 
 // Ethernet config
 //---------------------------------------------------------------------------------
@@ -27,67 +28,112 @@ struct cas {
   uint8_t min;
   uint8_t sec;
 };
-cas time;
 
-char c = 0;                   // citanie  retazcov poziadavky
-String readString;            // ukladanie retazcov poziadavky
+uint8_t ntp_delay = 0;
+char c = 0;               // citanie  retazcov poziadavky
+String readString;        // ukladanie retazcov poziadavky
 #define relay  7          // I/O rele
-#define led_on  13
-#define led_status  8
+#define led_on  6
 #define button  9
 bool relay_status = false;
-bool ethernet_s = false;
 bool button_s = false;
+cas a_time = {23,0,0};
+cas time;
+cas dhcp_time = {0,0,0};
+uint8_t akcia = 0;
+bool stav = false;
+File webFile;               // the web page file on the SD card
+
 //---------------------------------------------------------------------------------
 
 // function
 //---------------------------------------------------------------------------------
 void get_time(EthernetUDP *Udp,cas *time);
+unsigned long sendNTPpacket(IPAddress& address);
+void json_response(EthernetClient client);
 //---------------------------------------------------------------------------------
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Boot");
+  //Serial.println("start");
+  // disable Ethernet chip
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+  SD.begin(4);
+ /* Serial.println("Memmory card");
+  if (!SD.begin(4)) 
+    Serial.println("Failed initialize SD card"); */
   
-  Serial.println("Getting IP adres wia DHCP");
+  //Serial.println("Getting IP adres wia DHCP");
   //Ethernet.begin(mac, ip, brana, maska);
-  if (Ethernet.begin(mac) == 0)
-     Serial.println("Failed to configure Ethernet using DHCP");
+//  if (Ethernet.begin(mac) == 0)
+//     Serial.println("Failed to configure Ethernet using DHCP");
+     
+  Ethernet.begin(mac);
   Serial.println(Ethernet.localIP());
+  get_time(&Udp,&dhcp_time);
+  
   
   // Configuration digital I/O
-  Serial.println("Configuration pin");
   pinMode(relay,OUTPUT);
   pinMode(led_on,OUTPUT);
-  pinMode(led_status,OUTPUT);
   pinMode(button,INPUT);
-  digitalWrite(relay,LOW);
+  digitalWrite(relay,HIGH);
   digitalWrite(led_on,HIGH);
-  digitalWrite(led_status,LOW);
   button_s = digitalRead(button);
-  
-  Serial.println("Starting server");
-  // Starting NTP,WHTTP server, DHT sensor
+
+  // Starting NTP,HTTP server
   Udp.begin(localPort);
   server.begin();
-
-  Serial.println("Start");
 }
 
 void loop() {
+  delay(100);
   
-  if (digitalRead(button) != button_s)
-  {
-    digitalWrite(relay,!relay_status);
-    digitalWrite(led_status,!relay_status);
-    relay_status = !relay_status;
-    button_s = digitalRead(button);
-    Serial.println("Change status button");
-    Serial.print("Relay status: ");
-    Serial.println(relay_status);
+  if ((dhcp_time.hour == time.hour-1) && (dhcp_time.min == time.min)){
+    Ethernet.localIP();
+    get_time(&Udp,&dhcp_time);
   }
   
-  get_time(&Udp,&time);
+  if (ntp_delay >= 150) {
+    get_time(&Udp,&time);
+    ntp_delay=0;
+  }
+  else
+    ntp_delay++;
+/*   
+  Serial.print(time.hour);
+  Serial.print(":");
+  Serial.print(time.min);
+  Serial.print(":");
+  Serial.println(time.sec);
+  Serial.println("Slucka");
+  Serial.println(ntp_delay);
+*/
+  if (digitalRead(button) != button_s)
+  {
+    digitalWrite(relay,relay_status);
+    relay_status = !relay_status;
+    button_s = digitalRead(button);
+  }
+  
+  if ((time.hour == a_time.hour)&&(time.min == a_time.min)&&(!stav)){
+    //Serial.println("Cas sa naplnil");
+    stav = true;
+    if (akcia == 0)
+      digitalWrite(relay,LOW);
+    else if (akcia == 1)
+      digitalWrite(relay,HIGH);
+    else if (akcia == 2)
+      digitalWrite(relay,relay_status);
+  }
+  else if ((time.hour == a_time.hour)&&(time.min != a_time.min)&&(stav)) {
+    if (akcia == 2) 
+      digitalWrite(relay,!relay_status);
+    stav = false;
+  }
+    
+
   EthernetClient client = server.available();
   if (client) {
     while (client.connected()) {
@@ -95,61 +141,78 @@ void loop() {
         char c = client.read();
         //read char by char HTTP request
         if (readString.length() < 100) {
-          //store characters to string
           readString += c;
-          Serial.print(c);
+          //Serial.print(c);
         }
         //if HTTP request has ended
         if (c == '\n') {
-          Serial.println(readString); //print to serial monitor for debuging
-
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");
-     	  client.println("Refresh: 10");
-          client.println();
-          client.println("<!DOCTYPE HTML><HTML><HEAD><center>");
-          client.println("<TITLE>Home Automation</TITLE>");
-          client.println("</HEAD><BODY>");
-          client.println("<H1>Vzdialene ovladanie zasuvky</H1>");
-          client.println("<hr />");
-          client.println("Cas: ");
-          client.print(time.hour);
-          client.print(":");
-          client.print(time.min);
-          client.print(":");
-          client.print(time.sec);
-          client.println("<br/><br/>");
-          client.println("Status: ");
-          client.println((relay_status ? "Zapnuta" : "Vypnuta"));
-          client.println("<hr /><br />");
-          client.println((relay_status ? "<a href='http://192.168.7.59?relayoff'><button style='background:red;width:10%;height:40px'>Vypnut</button></a>" : 
-          "<a href='http://192.168.7.59?relayon'><button style='background:lightgreen;width:10%;height:40px'>Zapnut</button></a>"));          
-          client.println("</center></BODY></HTML>");
-          delay(1);
-          client.stop();
 
           if(readString.indexOf("?relayon") >0){
-            digitalWrite(led_status, HIGH);    // set pin 4 high
-            digitalWrite(relay,HIGH);
-            Serial.println("Led On");
+            digitalWrite(relay,LOW);
             relay_status=true;
           }
-          else{
+          else { 
             if(readString.indexOf("?relayoff") >0){
-              digitalWrite(led_status, LOW);    // set pin 4 low
-              digitalWrite(relay,LOW);
-              Serial.println("Led Off");
+              digitalWrite(relay,HIGH);
               relay_status=false;
             }
+            else{ 
+              if (readString.indexOf("?akcia") >0){
+                if (akcia >=3)
+                  akcia =0;
+                else
+                  akcia++;
+              }
+              else{ 
+                if (readString.indexOf("?hourinc") >0){
+                  a_time.hour++;
+                }
+                else {
+                  if (readString.indexOf("?hourdec") >0){
+                    a_time.hour--;
+                  }
+                  else {
+                    if (readString.indexOf("?mininc") >0){
+                      a_time.min+=10;
+                    }
+                    else {
+                      if (readString.indexOf("?mindec") >0){
+                        a_time.min-=10;
+                      }
+                    }
+                  } 
+                }
+              }
+            }
           }
+          
+          client.println("HTTP/1.1 200 OK");
+          if(readString.indexOf("json_input") >0)
+          {
+            client.println("Content-Type: application/json");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println();
+            json_response(client);
+          }
+          else {
+            client.println("Content-Type: text/html");
+            client.println("Connection: close");  // the connection will be closed after completion of the response
+            client.println();
+          
+            webFile = SD.open("index.htm");        // open web page file
+            if (webFile) {
+              while(webFile.available()) {
+              client.write(webFile.read()); // send web page to client
+              }
+            webFile.close();
+            }
+          }
+
+            
+          delay(10);
+          client.stop();
           //clearing string for next read
           readString="";
-          Serial.print(time.hour);
-          Serial.print(":");
-          Serial.print(time.min);
-          Serial.print(":");
-          Serial.println(time.sec);
         }
       }
     }
@@ -184,7 +247,7 @@ void get_time(EthernetUDP *Udp,cas *time) {
      unsigned long epoch = secsSince1900 - seventyYears;  
      // print Unix time:
 
-     time->hour = (epoch  % 86400L) / 3600 +2;
+     time->hour = (epoch  % 86400L) / 3600 +2 -1;
      time->min = (epoch  % 3600) / 60;
      time->sec = epoch %60;
    }
@@ -212,4 +275,39 @@ unsigned long sendNTPpacket(IPAddress& address){
    Udp.beginPacket(address, 123); //NTP requests are to port 123
    Udp.write(packetBuffer,NTP_PACKET_SIZE);
    Udp.endPacket(); 
+}
+
+void json_response(EthernetClient client)
+{
+  //cas
+  client.print("{\"hour\":\"");
+  client.print(time.hour);
+  client.print("\",\"min\":\"");
+  client.print(time.min);
+  client.print("\",\"sec\":\"");
+  client.print(time.sec);
+
+  // Akcia
+  client.print("\",\"akcia\":\"");
+  if (akcia == 0)
+    client.print("Zapnut");
+  else if (akcia == 1)
+    client.print("Vypnut");
+  else if (akcia == 2)
+    client.print("Restartovat");
+  else if (akcia == 3)
+    client.print("Ziadna akcia");
+    
+  // cas vypnutia
+  client.print("\",\"a_hour\":\"");
+  client.print(a_time.hour);
+  client.print("\",\"a_min\":\"");
+  client.print(a_time.min);
+  client.print("\",\"a_sec\":\"");
+  client.print(a_time.sec);         
+      
+  // stav rele
+  client.print("\",\"relatko\":\"");
+  client.print((relay_status ? "on" : "off"));          
+  client.print("\"}");
 }
